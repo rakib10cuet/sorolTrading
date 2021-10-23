0ffe227eb74c80c66068617cb2a30631b0fb2938
Rezwoan Ahmed <rezwoan.rakib@apsissolutions.com>
2021-10-23 23:31:27 +0600

    with backend

---
+++ backend/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id: '1',
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            image: '/images/products/niki.jpg',
+            price: 120,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 10,
+            description: 'High Quality product'
+        },
+        {
+            _id: '2',
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            image: '/images/products/adidas.jpg',
+            price: 100,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 12,
+            description: 'High Quality product'
+        },        {
+            _id: '3',
+            name: 'Puma Slim Shirt',
+            category: 'Shirts',
+            image: '/images/products/puma.jpg',
+            price: 50,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 18,
+            description: 'High Quality product'
+        },        {
+            _id: '4',
+            name: 'Nike Slim Pant',
+            category: 'Pant',
+            image: '/images/products/niki.jpg',
+            price: 480,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 100,
+            description: 'High Quality product'
+        },        {
+            _id: '5',
+            name: 'Adidas Slim Pant',
+            category: 'Pant',
+            image: '/images/products/adidas.jpg',
+            price: 550,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 11,
+            description: 'High Quality product'
+        },        {
+            _id: '6',
+            name: 'Puma Slim Pant',
+            category: 'Puma',
+            image: '/images/products/puma.jpg',
+            price: 520,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            countInStock: 0,
+            description: 'High Quality product'
+        }
+    ]
+}
+export default data;
